241fc25e7c09a41f5789fb2f33086dbda15d0ddb
Amr Elaraby <amrselaraby@gmail.com>
2024-04-09 16:01:03 +0200

    finished user_profile

--- foodon_main/settings.py
+++ foodon_main/settings.py
@@ -127,6 +127,10 @@ STATICFILES_DIRS = [
     "foodon_main/static",
 ]
 
+# Media files Configurations
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
